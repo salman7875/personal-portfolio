1e0be45db720b2dec0b54cf88a5d5d94b287335a
Salman Ansari <imsalman315@gmail.com>
2023-06-05 20:11:05 +0530

    Minor fix

--- src/pages/Pdf/PdfViewer.jsx
+++ src/pages/Pdf/PdfViewer.jsx
@@ -3,7 +3,6 @@ import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
 import './pdf.scss'
-import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 // Enable PDF.js worker
